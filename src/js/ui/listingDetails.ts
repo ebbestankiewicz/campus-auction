ff7205161156205f3858b31649cbd31482cd80e0
ebbestankiewcz <ebbe2002@me.com>
2026-03-24 23:04:09 +0100

    feat: enhance listing details layout with improved styling and structure #5

--- src/js/ui/listingDetails.ts
+++ src/js/ui/listingDetails.ts
@@ -17,7 +17,7 @@ export function createListingDetails(listing: Listing): string {
     ? listing.bids
         .map(
           (bid) => `
-            <li>
+            <li class="border rounded-lg mt-4 p-4 flex justify-between">
               <span>${bid.bidder.name}</span>
               <span>${bid.amount}</span>
             </li>
@@ -27,37 +27,41 @@ export function createListingDetails(listing: Listing): string {
     : `<li>No bids yet</li>`;
 
   return `
-    <article>
+    <article class="mx-auto max-w-[800px] p-6 text-white border border-slate-700 shadow-md">
       <div>
         <div>
-          <img src="${image}" alt="${alt}" />
+          <img src="${image}" alt="${alt}" class="rounded-2xl border border-slate-600 bg-white" />
         </div>
 
         <div>
-          <div>
-            <h1>${listing.title}</h1>
-            <p>
+          <div class="mt-6">
+            <h1 class="text-2xl font-bold">${listing.title}</h1>
+            <p class="mt-4 text-sm">
               ${listing.description || "No description available."}
             </p>
           </div>
 
-          <div>
-            <h2>Seller</h2>
-            <p>Name: ${sellerName}</p>
-            <p>Email: ${sellerEmail}</p>
+          <div class="mt-6">
+            <h2 class="text-xl font-semibold">Seller</h2>
+            <div class=" border p-4 rounded-lg mt-2">
+                <p>Name: ${sellerName}</p>
+                <p>Email: ${sellerEmail}</p>
+            </div>
           </div>
 
-          <div>
-            <h2>Auction info</h2>
-            <p>Total bids: ${listing._count?.bids ?? 0}</p>
-            <p>Highest bid: ${highestBid}</p>
-            <p>Ends: ${new Date(listing.endsAt).toLocaleString()}</p>
+          <div class="mt-6">
+            <h2 class="text-xl font-semibold">Auction info</h2>
+            <div class=" border p-4 rounded-lg mt-2">
+              <p>Total bids: ${listing._count?.bids ?? 0}</p>
+              <p>Highest bid: ${highestBid}</p>
+              <p>Ends: ${new Date(listing.endsAt).toLocaleString()}</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div>
-        <h2>Bid history</h2>
+      <div class="mt-6">
+        <h2 class="text-xl font-semibold">Bid history</h2>
         <ul>
           ${bidsHtml}
         </ul>
